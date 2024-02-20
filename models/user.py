42d3fbcf0bd87f9932cb1bd0281c32b135ec16bd
houssam980 <houssam.ghmari1243@gmail.com>
2024-02-20 08:47:36 -0800

    updating user

--- models/user.py
+++ models/user.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-"""
-define user class's module
-"""
-from models.base_model import BaseModel
+"""user module"""
+
+import models
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from os import getenv
 
 
-class User(BaseModel):
-    """
-    user instance
-    """
+class User(BaseModel, Base):
+    """User creation"""
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
+    def __init__(self, *args, **kwargs):
+        """user initializing"""
+        super().__init__(*args, **kwargs)
