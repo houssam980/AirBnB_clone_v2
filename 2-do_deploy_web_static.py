1e7a4845e81bd3acc365d9765a2fd72f8712d65f
houssam980 <houssam.ghmari1243@gmail.com>
2024-03-07 07:40:26 -0800

    im getting tired

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
 """
-
+generate tgz
 """
-from os.path import exists
-from fabric.api import put, run, env
 
-env.hosts = ['100.26.49.131', '54.197.89.103']
+from datetime import datetime
+from os.path import isdir
+from fabric.api import local
 
 
-def do_deploy(archive_path):
-    if exists(archive_path) is False:
-        return False
+def do_pack():
     try:
-        f_name = archive_path.split("/")[-1]
-        not_exct = f_name.split(".")[0]
-        pth = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(pth, not_exct))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(f_name, pth, not_exct))
-        run('rm /tmp/{}'.format(f_name))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(pth, not_exct))
-        run('rm -rf {}{}/web_static'.format(pth, not_exct))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(pth, not_exct))
-        return True
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
     except:
-        return False
+        return None
