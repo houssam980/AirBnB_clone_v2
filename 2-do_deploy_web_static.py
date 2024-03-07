ed87cb439cfe5f994eafea90f8b2a69c142b0aca
houssam980 <houssam.ghmari1243@gmail.com>
2024-03-07 07:46:43 -0800

    im getting tired

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,22 +1,37 @@
 #!/usr/bin/python3
-"""
-Fabric script that generates a tgz archive from the contents of the web_static
-folder of the AirBnB Clone repo
-"""
-
-from datetime import datetime
-from fabric.api import local
-from os.path import isdir
 
 
-def do_pack():
-    """generates a tgz archive"""
+from fabric.api import *
+import os
+
+env.hosts = ['100.26.49.131', '54.197.89.103'] 
+
+def do_deploy(archive_path):
+    """Distributes an archive to web servers."""
+
+    if not os.path.isfile(archive_path):
+        return False
+
     try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except:
-        return None
+        archive_name = os.path.basename(archive_path)
+        release_name = archive_name.split('.')[0]
+
+        put(archive_path, '/tmp/{}'.format(archive_name))
+
+        with cd('/data/web_static/releases'):
+            run('mkdir -p {}'.format(release_name))  # Ensure directory exists
+            run('tar -xzf /tmp/{} -C {}'.format(archive_name, release_name))
+
+        run('rm /tmp/{}'.format(archive_name))
+        run('mv {}/web_static/* {}/'.format(release_name, release_name))
+        run('rm -rf {}/web_static'.format(release_name))
+
+        run('rm -rf /data/web_static/current')
+        run('ln -sf /data/web_static/releases/{} /data/web_static/current'.format(release_name))
+
+        return True
+
+    except Exception as e:
+        print("Error during deployment:", e)
+        return False
+
