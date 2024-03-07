94a842db24bc58a99c11d59ff1592acb3f1dad16
houssam980 <houssam.ghmari1243@gmail.com>
2024-03-07 07:41:44 -0800

    im getting tired

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """
-generate tgz
+Fabric script that generates a tgz archive from the contents of the web_static
+folder of the AirBnB Clone repo
 """
 
 from datetime import datetime
-from os.path import isdir
 from fabric.api import local
+from os.path import isdir
 
 
 def do_pack():
+    """generates a tgz archive"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
